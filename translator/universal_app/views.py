0a116ff3899009f7da4658044c711d6418122aef
rumiFlea <132996945+rumiFlea@users.noreply.github.com>
2023-05-12 16:28:39 +0200

    Update views.py

--- translator/universal_app/views.py
+++ translator/universal_app/views.py
@@ -1,9 +1,17 @@
-from django.views.generic import FormView
 from django.urls import reverse_lazy
+from django.views.generic import FormView
 from .forms import SpeechToTextForm
+from faster_whisper import WhisperModel
 import whisper
+import os, shutil
 import torch
 import subprocess
+
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_DIR = os.path.join(os.path.dirname(FILE_DIR), "outputs")
+
+def download_all_files():
+        shutil.make_archive("outputs", "txt", OUTPUT_DIR)
 
 class SpeechToTextView(FormView):
     template_name = 'index.html'
@@ -12,35 +20,38 @@ class SpeechToTextView(FormView):
 
     def form_valid(self, form):
         audio_file = form.cleaned_data['audio']
-        model = whisper.load_model("medium")
-        
-        file_path = "/tmp/" + str(audio_file)
+        model = whisper.load_model("base")
+
+        file_path = "/tmp/" + audio_file.name
         with open(file_path, 'wb+') as destination:
             for chunk in audio_file.chunks():
                 destination.write(chunk)
 
-        audio = whisper.load_audio(file_path)
-        audio = whisper.pad_or_trim(audio)
-        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+        
 
-        _, probs = model.detect_language(mel)
-        language = max(probs, key=probs.get)
+        model_size = "base"
 
-        # Run whisper command with arguments for translation
-        command = ["whisper", file_path, "--language", language, "--task", "translate"]
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-        # Capture output and errors
-        output, error = process.communicate()
-        english_text = output.decode()
+        segments, info = model.transcribe(file_path, beam_size=5, task="translate")
+        text = ''
+        for segment in segments:
+            #text.append(segment.text)
+            text = text + segment.text
 
-        # Handle any errors
-        if process.returncode != 0:
-            print(f"Error: {error.decode()}")
-            english_text = "An error occurred during translation."
+
+        # run whisper command with arguments
+        # command = ["whisper", f"{audio}", "--language", "Japanese", "--task", "translate"]
+        # process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # # capture output and errors
+        # output, error = process.communicate()
+
+        # context = output.decode()
 
         context = self.get_context_data()
-        context['language'] = language
-        context['text'] = english_text
+        context['language'] = info[0]
+        context['text'] = text
+        # TODO: save the text to a file
 
         return self.render_to_response(context)
